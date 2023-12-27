4a86786957a0d0d9cc04b67e0d564819d702208c
Monyjenni <chansovanmonyyoeun03@gmail.com>
2023-12-27 23:24:15 +0700

    Removed_jsonify

--- Api_MySQL/app.py
+++ Api_MySQL/app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # Configure the Flask app for SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456789@localhost:3306/apdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:012345678@localhost:3306/apdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Create an instance of the SQLAlchemy extension
@@ -29,9 +29,7 @@ def get_terms():
         }
         for term in results
     ]
-
-    # Return the list as JSON
-    return jsonify(terms_list)
+    return terms_list
 
 @app.route('/terms1')
 def get_terms1():
@@ -41,8 +39,7 @@ def get_terms1():
     # Use _asdict() to convert each result to a dictionary
     terms_list = [term._asdict() for term in results]
 
-    # Return the list as JSON
-    return jsonify(terms_list)
+    return terms_list
 
 if __name__ == '__main__':
     # Run the Flask app
